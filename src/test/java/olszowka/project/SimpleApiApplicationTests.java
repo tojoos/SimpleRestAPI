f675d51729c6ca525d7aefdfd79a7761835e6ee9
ToJoos <joosx2000@gmail.com>
2022-02-23 11:46:07 +0100

    Initial commit

---
+++ src/test/java/olszowka/project/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package olszowka.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
