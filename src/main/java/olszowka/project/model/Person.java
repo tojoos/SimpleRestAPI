f2f9818a2d13d90a5d0630b8552c35c9209961a7
ToJoos <joosx2000@gmail.com>
2022-02-23 12:34:03 +0100

    Code styling

--- src/main/java/olszowka/project/model/Person.java
+++ src/main/java/olszowka/project/model/Person.java
@@ -8,14 +8,13 @@ import java.util.UUID;
 
 @Getter @Setter
 public class Person {
+
     private UUID id;
     private String name;
-
 
     public Person(@JsonProperty("id") UUID id,
                   @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
-
 }
